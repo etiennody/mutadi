770afadcff33765ae358092e23747c748c7ebe3b
etiennody <etiennody@gmail.com>
2020-12-04 02:44:34 +0100

    add first models

--- mutadi/posts/models.py
+++ mutadi/posts/models.py
@@ -1,3 +1,37 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
-# Create your models here.
+User = get_user_model()
+
+
+class Category(models.Model):
+    title = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.title
+
+
+class Comment(models.Model):
+    content = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(
+        "Post", related_name="comments", on_delete=models.CASCADE
+    )
+
+    def __str__(self):
+        return self.user.username
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    overview = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+    content = models.TextField()
+    thumbnail = models.ImageField()
+    featured = models.BooleanField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    categories = models.ManyToManyField(Category)
+
+    def __str__(self):
+        return self.title + " | " + str(self.author)
