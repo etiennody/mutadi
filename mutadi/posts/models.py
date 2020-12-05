9dc801f4d6ef2b9af278faad57d338c9d19830a9
etiennody <etiennody@gmail.com>
2020-12-05 12:36:05 +0100

    register post and category models with the admin

--- mutadi/posts/models.py
+++ mutadi/posts/models.py
@@ -8,6 +8,9 @@ class Category(models.Model):
     def __str__(self):
         return self.title
 
+    class Meta:
+        verbose_name_plural = "categories"
+
 
 class Comment(models.Model):
     content = models.TextField()
--- mutadi/posts/tests/test_models.py
+++ mutadi/posts/tests/test_models.py
@@ -7,39 +7,52 @@ from mutadi.posts.models import Category, Comment, Post
 pytestmark = pytest.mark.django_db
 
 
-def test___str__category_model():
-    """Test __str__() method in Category model"""
-    category = Category.objects.create(title="Bricolage")
-    assert category.__str__() == "Bricolage"
-    assert str(category) == "Bricolage"
+class TestCategoryClass:
+    """Group multiple tests in Category class"""
+
+    def test___str__category_model(self):
+        """Test __str__() method in Category model"""
+        category = Category.objects.create(title="Bricolage")
+        assert category.__str__() == "Bricolage"
+        assert str(category) == "Bricolage"
+
+    def test_verbose_name_plural_categories(self):
+        """Test verbose_name_plural for categories"""
+        assert Category._meta.verbose_name_plural == "categories"
 
 
-def test___str__comment_model():
-    """Test __str__() method in Category model"""
-    user = User.objects.create(username="Bob")
-    post = Post.objects.create(
-        title="Post1",
-        overview="This an overview1",
-        content="This a content1",
-        featured=True,
-        author=user,
-    )
-    comment = Comment.objects.create(
-        content="This a content", user=user, post=post
-    )
-    assert comment.__str__() == "Bob"
-    assert str(comment) == "Bob"
+class TestCommentClass:
+    """Group multiple tests in Comment class"""
+
+    def test___str__comment_model(self):
+        """Test __str__() method in Category model"""
+        user = User.objects.create(username="Bob")
+        post = Post.objects.create(
+            title="Post1",
+            overview="This an overview1",
+            content="This a content1",
+            featured=True,
+            author=user,
+        )
+        comment = Comment.objects.create(
+            content="This a content", user=user, post=post
+        )
+        assert comment.__str__() == "Bob"
+        assert str(comment) == "Bob"
 
 
-def test___str__post_model():
-    """Test __str__() method in Category model"""
-    user = User.objects.create(username="Bob")
-    post = Post.objects.create(
-        title="Post2",
-        overview="This an overview2",
-        content="This a content2",
-        featured=True,
-        author=user,
-    )
-    assert post.__str__() == "Post2 | Bob"
-    assert str(post) == "Post2 | Bob"
+class TestPostClass:
+    """Group multiple tests in Post class"""
+
+    def test___str__post_model(self):
+        """Test __str__() method in Category model"""
+        user = User.objects.create(username="Bob")
+        post = Post.objects.create(
+            title="Post2",
+            overview="This an overview2",
+            content="This a content2",
+            featured=True,
+            author=user,
+        )
+        assert post.__str__() == "Post2 | Bob"
+        assert str(post) == "Post2 | Bob"
