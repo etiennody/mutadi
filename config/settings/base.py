be12409ae53379174f9c5205e398ea4f3365f174
etiennody <etiennody@gmail.com>
2020-12-07 13:39:58 +0100

    add pipenv run on script

--- config/settings/base.py
+++ config/settings/base.py
@@ -98,7 +98,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
-LANGUAGE_CODE = "fr"
+LANGUAGE_CODE = os.environ["DJANGO_LANGUAGE_CODE"]
 
 TIME_ZONE = os.environ["DJANGO_TIME_ZONE"]
 
