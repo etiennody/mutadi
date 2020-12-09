266f3e043e20188e3cf9520326d9dd1d79cd9d6f
etiennody <etiennody@gmail.com>
2020-12-09 14:54:16 +0100

    modify urls for django auth urls

--- config/urls.py
+++ config/urls.py
@@ -8,7 +8,6 @@ from django.urls import include, path
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("mutadi.pages.urls")),
-    path("members/", include("django.contrib.auth.urls")),
     path("members/", include("mutadi.members.urls")),
 ]
 
--- mutadi/members/urls.py
+++ mutadi/members/urls.py
@@ -1,8 +1,9 @@
 """members URL Configuration
 """
-from django.urls import path
+from django.urls import path, include
 from .views import user_register_view
 
 urlpatterns = [
     path("register/", user_register_view, name="register"),
+    path("", include("django.contrib.auth.urls")),
 ]
