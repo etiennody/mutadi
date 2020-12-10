bb228f955f42d6d3fbaa75b2a85b9cef171d9d74
etiennody <etiennody@gmail.com>
2020-12-11 00:33:04 +0100

    create PostDetailView

--- mutadi/posts/views.py
+++ mutadi/posts/views.py
@@ -1,13 +1,24 @@
 """posts Views Configuration"""
-from django.views.generic import ListView
+from django.views.generic import DetailView, ListView
+
 from .models import Post
 
 
 class PostListView(ListView):
     """Post list view"""
 
-    model = Post
+    queryset = Post.objects.filter(status=1).order_by("-created_on")
     template_name = "posts/post_list.html"
 
 
 post_list_view = PostListView.as_view()
+
+
+class PostDetailView(DetailView):
+    """Post detail view"""
+
+    model = Post
+    template_name = "posts/post_detail.html"
+
+
+post_detail_view = PostDetailView.as_view()
