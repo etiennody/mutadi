2e2d2640bf5ecdcde6e69cea25711afa69ca0d9c
etiennody <etiennody@gmail.com>
2020-12-11 15:13:58 +0100

    fix posts/urls, posts/views and pages/test_views

--- mutadi/posts/views.py
+++ mutadi/posts/views.py
@@ -8,7 +8,7 @@ class PostListView(ListView):
     """Post list view"""
 
     queryset = Post.objects.filter(status=1).order_by("-created_on")
-    template_name = "posts/post_list.html"
+    template_name = "post_list.html"
 
 
 post_list_view = PostListView.as_view()
@@ -18,7 +18,7 @@ class PostDetailView(DetailView):
     """Post detail view"""
 
     model = Post
-    template_name = "posts/post_detail.html"
+    template_name = "post_detail.html"
 
 
 post_detail_view = PostDetailView.as_view()
