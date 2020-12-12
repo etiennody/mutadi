6f1bb72e159938caa217db8fad79f145d3059531
etiennody <etiennody@gmail.com>
2020-12-12 10:32:53 +0100

    update with add_post/ url

--- mutadi/posts/urls.py
+++ mutadi/posts/urls.py
@@ -2,9 +2,10 @@
 """
 from django.urls import path
 
-from .views import post_list_view, post_detail_view
+from .views import add_post_view, post_detail_view, post_list_view
 
 urlpatterns = [
     path("post_list/", post_list_view, name="post_list"),
     path("post_detail/<int:pk>", post_detail_view, name="post_detail"),
+    path("add_post/", add_post_view, name="add_post"),
 ]
