8c63f8777a60941c988285c3da431ff3cf74afd0
etiennody <etiennody@gmail.com>
2020-12-12 12:22:01 +0100

    add get_absolute_url() reverse to home

--- mutadi/posts/models.py
+++ mutadi/posts/models.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import User
 from django.db import models
+from django.urls import reverse
 
 
 class Category(models.Model):
@@ -44,3 +45,6 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title + " | " + str(self.author)
+
+    def get_absolute_url(self):
+        return reverse("home")
