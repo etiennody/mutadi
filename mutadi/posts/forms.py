d6849e93676712915a10e8bc63aa785f9cc5494b
etiennody <etiennody@gmail.com>
2020-12-14 00:02:59 +0100

    add EditForm

--- mutadi/posts/forms.py
+++ mutadi/posts/forms.py
@@ -28,3 +28,36 @@ class PostForm(forms.ModelForm):
             "featured": "Mise en avant",
             "status": "Statut",
         }
+
+
+class EditForm(forms.ModelForm):
+    """Update post form"""
+
+    class Meta:
+        model = Post
+        fields = (
+            "title",
+            "categories",
+            "overview",
+            "content",
+            "thumbnail",
+            "featured",
+            "status",
+        )
+        widgets = {
+            "title": forms.TextInput(attrs={"class": "form-control"}),
+            "categories": forms.CheckboxSelectMultiple,
+            "overview": forms.TextInput(attrs={"class": "form-control"}),
+            "content": forms.Textarea(attrs={"class": "form-control"}),
+            "featured": forms.CheckboxInput,
+            "status": forms.Select(attrs={"class": "form-control"}),
+        }
+        labels = {
+            "title": "Titre",
+            "categories": "Catégories",
+            "overview": "Présentation",
+            "content": "Contenu",
+            "thumbnail": "Image",
+            "featured": "Mise en avant",
+            "status": "Statut",
+        }
