b7ce1723d9e7a9b33a9e791b8d8e33413afdd49e
etiennody <etiennody@gmail.com>
2020-12-18 12:21:23 +0100

    add edit_profile/ url

--- mutadi/members/urls.py
+++ mutadi/members/urls.py
@@ -1,9 +1,11 @@
 """members URL Configuration
 """
-from django.urls import path, include
-from .views import user_register_view
+from django.urls import include, path
+
+from .views import user_register_view, user_edit_view
 
 urlpatterns = [
     path("register/", user_register_view, name="register"),
     path("", include("django.contrib.auth.urls")),
+    path("edit_profile/", user_edit_view, name="edit_profile"),
 ]
