6ee30aa3c25ae6a694bb3c9f8a3811c3fbf6eb92
etiennody <etiennody@gmail.com>
2020-12-18 12:18:59 +0100

    create UserEditView

--- mutadi/members/views.py
+++ mutadi/members/views.py
@@ -1,9 +1,8 @@
 """members Views Configuration"""
-from django.contrib.auth.forms import UserCreationForm
 from django.contrib.messages.views import SuccessMessageMixin
 from django.urls import reverse_lazy
 from django.views import generic
-from .forms import SignUpForm
+from .forms import SignUpForm, EditProfileForm
 
 
 class UserRegisterView(SuccessMessageMixin, generic.CreateView):
@@ -16,3 +15,18 @@ class UserRegisterView(SuccessMessageMixin, generic.CreateView):
 
 
 user_register_view = UserRegisterView.as_view()
+
+
+class UserEditView(SuccessMessageMixin, generic.UpdateView):
+    """User registration view"""
+
+    form_class = EditProfileForm
+    template_name = "registration/edit_profile.html"
+    success_url = reverse_lazy("home")
+    success_message = "Le profil a été modifiéé avec succès !"
+
+    def get_object(self):
+        return self.request.user
+
+
+user_edit_view = UserEditView.as_view()
