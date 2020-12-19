d2d2141f51e8f39e95d0091dbac36d920c29ccd2
etiennody <etiennody@gmail.com>
2020-12-19 23:05:01 +0100

    add change_password/ and change_password_success urls

--- mutadi/members/urls.py
+++ mutadi/members/urls.py
@@ -2,10 +2,21 @@
 """
 from django.urls import include, path
 
-from .views import user_register_view, user_edit_view
+from .views import (
+    change_password_success,
+    change_password_view,
+    user_edit_view,
+    user_register_view,
+)
 
 urlpatterns = [
     path("register/", user_register_view, name="register"),
     path("", include("django.contrib.auth.urls")),
     path("edit_profile/", user_edit_view, name="edit_profile"),
+    path("password/", change_password_view, name="change_password"),
+    path(
+        "change_password_success/",
+        change_password_success,
+        name="change_password_success",
+    ),
 ]
