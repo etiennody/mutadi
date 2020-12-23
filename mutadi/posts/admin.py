be27bacfa940013139a11a6a3f81747158e1d3bb
etiennody <etiennody@gmail.com>
2020-12-23 19:14:32 +0100

    add Comment in django system admin

--- mutadi/posts/admin.py
+++ mutadi/posts/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from .models import Category, Post, Profile
+from .models import Category, Comment, Post, Profile
 
 admin.site.register(Post)
 admin.site.register(Category)
 admin.site.register(Profile)
+admin.site.register(Comment)
