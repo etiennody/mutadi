8914a0a77ee917cb51e390ab98b3a052fcab5b0d
etiennody <etiennody@gmail.com>
2020-12-26 21:26:07 +0100

    add context_data for featured_posts and latests_posts

--- mutadi/pages/views.py
+++ mutadi/pages/views.py
@@ -6,8 +6,16 @@ from mutadi.posts.models import Post
 class HomeView(ListView):
     """Home page view"""
 
+    model = Post
     template_name = "pages/home.html"
-    queryset = Post.objects.filter(featured=True)
+
+    def get_context_data(self, **kwargs):
+        featured_posts = Post.objects.filter(featured=True)[:3]
+        latest_posts = Post.objects.order_by("-created_on")[:3]
+        context = super().get_context_data(**kwargs)
+        context["featured_posts"] = featured_posts
+        context["latest_posts"] = latest_posts
+        return context
 
 
 home_view = HomeView.as_view()
