21e7099237e227a555cbec63a5902ad95ab291e6
etiennody <etiennody@gmail.com>
2020-12-27 22:44:08 +0100

    add InboxView

--- mutadi/private_messages/views.py
+++ mutadi/private_messages/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+"""Private Messages Views Configuration"""
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import ListView
 
-# Create your views here.
+from .models import PrivateMessage
+
+
+class InboxView(LoginRequiredMixin, ListView):
+    """Returns all messages that were received by the given user"""
+
+    model = PrivateMessage
+    template_name = "inbox.html"
+    object_context_name = "message_list"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        message_list = PrivateMessage.objects.filter(
+            recipient=self.request.user
+        )
+        context["message_list"] = message_list
+        return context
+
+
+inbox_view = InboxView.as_view()
