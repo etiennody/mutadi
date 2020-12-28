03f21948b81f85960a27ed0a7a38fbfd749df78e
etiennody <etiennody@gmail.com>
2020-12-28 09:34:07 +0100

    add urls and views outbox tests

--- mutadi/private_messages/tests/test_views.py
+++ mutadi/private_messages/tests/test_views.py
@@ -71,3 +71,65 @@ class TestInboxViews:
         )
         response = client.get(reverse("inbox"))
         assert response.context_data["message_list"].count() == 0
+
+
+class TestOutboxViews:
+    """Group multiple tests in Outbox views"""
+
+    @pytest.fixture
+    def proto_user(self):
+        """Fixture for baked User model."""
+        self.proto_user = baker.make(User)
+        self.proto_user.set_password("m=9UaK^C,Tbq9N=T")
+        self.proto_user.save()
+        return self.proto_user
+
+    def test_view_url_outbox_page_exists_at_desired_location(
+        self, client, proto_user
+    ):
+        """outbox page should exist at desired location."""
+        client.login(
+            username=f"{proto_user.username}",
+            password="m=9UaK^C,Tbq9N=T",
+        )
+        response = client.get("/messages/outbox/")
+        assert response.status_code == 200
+
+    def test_view_url_accessible_by_name(self, client, proto_user):
+        """outbox page should be accessible by name."""
+        client.login(
+            username=f"{proto_user.username}",
+            password="m=9UaK^C,Tbq9N=T",
+        )
+        url = reverse("outbox")
+        response = client.get(url)
+        assert response.status_code == 200
+
+    def test_valid_outbox_page_title_with_client(self, client, proto_user):
+        """outbox page should contain "Publications"."""
+        client.login(
+            username=f"{proto_user.username}",
+            password="m=9UaK^C,Tbq9N=T",
+        )
+        url = reverse("outbox")
+        response = client.get(url)
+        assert "Publications" in str(response.content)
+
+    def test_view_outbox_page_uses_correct_template(self, client, proto_user):
+        """outbox page should use outbox.html template."""
+        client.login(
+            username=f"{proto_user.username}",
+            password="m=9UaK^C,Tbq9N=T",
+        )
+        response = client.get(reverse("outbox"))
+        assert response.status_code == 200
+        assertTemplateUsed(response, "outbox.html")
+
+    def test_view_message_list_context_is_ko(self, client, proto_user):
+        """Inbow should have no messages"""
+        client.login(
+            username=f"{proto_user.username}",
+            password="m=9UaK^C,Tbq9N=T",
+        )
+        response = client.get(reverse("outbox"))
+        assert response.context_data["message_list"].count() == 0
