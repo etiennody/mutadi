a4f88164dfdb71a2594dd9e3e8bbd09c8f2c0d71
etiennody <etiennody@gmail.com>
2020-12-30 00:28:35 +0100

    add reply message form tests

--- mutadi/private_messages/tests/test_forms.py
+++ mutadi/private_messages/tests/test_forms.py
@@ -3,7 +3,7 @@
 import pytest
 from django.contrib.auth.models import User
 from model_bakery import baker
-from mutadi.private_messages.forms import ComposeForm
+from mutadi.private_messages.forms import ComposeForm, ReplyForm
 from mutadi.private_messages.models import PrivateMessage
 
 pytestmark = pytest.mark.django_db
@@ -104,6 +104,84 @@ class TestComposeForm:
             "content": "",
         }
         form = ComposeForm(data)
+        assert not form.is_valid()
+        assert len(form.errors) == 1
+        assert "content" in form.errors
+
+
+class TestReplyForm:
+    """Group multiple tests for ReplyForm"""
+
+    @pytest.fixture
+    def proto_user_a(self):
+        """Fixture for baked User model."""
+        self.proto_user_a = baker.make(User)
+        self.proto_user_a.set_password("m=9UaK^C,Tbq9N=T")
+        self.proto_user_a.save()
+        return self.proto_user_a
+
+    @pytest.fixture
+    def proto_user_b(self):
+        """Fixture for baked User model."""
+        self.proto_user_b = baker.make(User)
+        self.proto_user_b.set_password("3$0aF/gxFsinR'6k")
+        self.proto_user_b.save()
+        return self.proto_user_b
+
+    @pytest.fixture
+    def proto_user_c(self):
+        """Fixture for baked User model."""
+        self.proto_user_c = baker.make(User)
+        self.proto_user_c.set_password("BJw_KkB&asjX_#B3")
+        self.proto_user_c.save()
+        return self.proto_user_c
+
+    @pytest.fixture
+    def proto_private_message(self, proto_user_a, proto_user_b):
+        """Fixture for baked PrivateMessage model."""
+        return baker.make(
+            PrivateMessage,
+            sender=proto_user_a,
+            recipient=proto_user_b,
+            content=(
+                "Proident nisi cillum sit tempor "
+                "reprehenderit proident in non fugiat ex id."
+            ),
+        )
+
+    def test_valid_reply_message_form(self):
+        """Reply message form should be valid for a member."""
+        data = {
+            "subject": "This is the subject",
+            "content": (
+                "Culpa est et aliquip non tempor "
+                "mollit exercitation cillum et."
+            ),
+        }
+        form = ReplyForm(data)
+        assert form.is_valid()
+
+    def test_invalid_reply_message_form_wit_subject_missing(self):
+        """Reply message form should be refused with subject missing."""
+        data = {
+            "subject": "",
+            "content": (
+                "Culpa est et aliquip non tempor "
+                "mollit exercitation cillum et."
+            ),
+        }
+        form = ReplyForm(data)
+        assert not form.is_valid()
+        assert len(form.errors) == 1
+        assert "subject" in form.errors
+
+    def test_invalid_reply_message_form_with_content_missing(self):
+        """Reply message form should be refused with content missing."""
+        data = {
+            "subject": "This is the subject",
+            "content": "",
+        }
+        form = ReplyForm(data)
         assert not form.is_valid()
         assert len(form.errors) == 1
         assert "content" in form.errors
