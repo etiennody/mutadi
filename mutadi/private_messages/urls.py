5519cea16afa8381983cb9d7900d009fec02296a
etiennody <etiennody@gmail.com>
2020-12-30 23:28:06 +0100

    add delete_message fbv in import+path

--- mutadi/private_messages/urls.py
+++ mutadi/private_messages/urls.py
@@ -4,7 +4,7 @@ from django.urls import path
 
 from .views import (
     compose_message_view,
-    delete_message_view,
+    delete_message,
     inbox_view,
     message_detail_view,
     outbox_view,
@@ -15,7 +15,7 @@ urlpatterns = [
     path("outbox/", outbox_view, name="outbox"),
     path(
         "message_detail/<int:pk>/delete",
-        delete_message_view,
+        delete_message,
         name="delete_message",
     ),
     path(
