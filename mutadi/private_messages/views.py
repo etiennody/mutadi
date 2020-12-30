e6e7eb6ec9f32268353462a55325937c85a63e21
etiennody <etiennody@gmail.com>
2020-12-30 23:26:24 +0100

    replace delete cbv to fbv and update inbox+outbox views

--- mutadi/private_messages/views.py
+++ mutadi/private_messages/views.py
@@ -1,9 +1,12 @@
 """Private Messages Views Configuration"""
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.messages.views import SuccessMessageMixin
-from django.urls import reverse_lazy
-from django.views.generic import CreateView, DeleteView, ListView
+from django.shortcuts import get_object_or_404, render
+from django.urls import reverse, reverse_lazy
+from django.utils import timezone
+from django.views.generic import CreateView, ListView
 
 from .forms import ComposeForm, ReplyForm
 from .models import PrivateMessage
@@ -12,16 +15,18 @@ from .models import PrivateMessage
 class InboxView(LoginRequiredMixin, ListView):
     """Returns all messages that were received by the given user"""
 
-    model = PrivateMessage
     template_name = "inbox.html"
-    object_context_name = "message_list"
+
+    def get_queryset(self):
+        self.message_list = PrivateMessage.objects.filter(
+            recipient=self.request.user,
+            recipient_deleted_at__isnull=True,
+        ).order_by("-sent_at")
+        return self.message_list
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        message_list = PrivateMessage.objects.filter(
-            recipient=self.request.user
-        ).order_by("-sent_at")
-        context["message_list"] = message_list
+        context["message_list"] = self.message_list
         return context
 
 
@@ -31,40 +36,52 @@ inbox_view = InboxView.as_view()
 class OutboxView(LoginRequiredMixin, ListView):
     """Returns all messages that were sent by the given user"""
 
-    model = PrivateMessage
     template_name = "outbox.html"
-    object_context_name = "message_list"
+
+    def get_queryset(self):
+        self.message_list = PrivateMessage.objects.filter(
+            sender=self.request.user,
+            sender_deleted_at__isnull=True,
+        ).order_by("-sent_at")
+        return self.message_list
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        message_list = PrivateMessage.objects.filter(
-            sender=self.request.user
-        ).order_by("-sent_at")
-        context["message_list"] = message_list
+        context["message_list"] = self.message_list
         return context
 
 
 outbox_view = OutboxView.as_view()
 
 
-class DeleteMessageView(LoginRequiredMixin, SuccessMessageMixin, DeleteView):
-    """Delete message view"""
-
-    model = PrivateMessage
-    template_name = "delete_message.html"
-    context_object_name = "message"
-    success_url = reverse_lazy("inbox")
-    success_message = "Le message a été supprimé avec succès !"
-
-    def delete(self, request, *args, **kwargs):
-        messages.warning(self.request, self.success_message)
-        return super(DeleteMessageView, self).delete(request, *args, **kwargs)
-
-
-delete_message_view = DeleteMessageView.as_view()
+@login_required(login_url="/members/login/")
+def delete_message(request, pk, success_url=None):
+    """
+    Marks a message as deleted by sender or recipient. The message is not
+    really removed from the database, because two users must delete a message
+    before it's save to remove it completely.
+    """
+    user = request.user
+    now = timezone.now()
+    message = get_object_or_404(PrivateMessage, pk=pk)
+    deleted = False
+    if success_url is None:
+        success_url = reverse("inbox")
+    if message.sender == user:
+        message.sender_deleted_at = now
+        deleted = True
+    if message.recipient == user:
+        message.recipient_deleted_at = now
+        deleted = True
+    if deleted:
+        message.save()
+        messages.warning(request, ("Message supprimé avec succès !"))
+    return render(request, "inbox.html", {"deleted": deleted})
 
 
 class MessageDetailView(LoginRequiredMixin, SuccessMessageMixin, CreateView):
+    """Message detail view and processing reply message"""
+
     model = PrivateMessage
     template_name = "message_detail.html"
     form_class = ReplyForm
